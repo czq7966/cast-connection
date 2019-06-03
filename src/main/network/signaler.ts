49fc7ba4016e6b362995019a29223a44305acd3d
陈志谦 <761208@nd.com>
2019-06-03 14:32:11 +0800

    增加信令连接工厂模式

--- src/main/network/signaler.ts
+++ src/main/network/signaler.ts
@@ -1,5 +1,44 @@
 import { Client } from './client'; 
 
-export class Signaler extends Client {
+export var DefaultFactorySignalerName = "DefaultFactorySignaler";
+export interface ISignaler {
+    destroy();
+    id(): string;
+    connected(): boolean;
+    connect(url?: string): Promise<any>;
+    disconnect();
+    connecting(): boolean;
+}
+
+export interface ISignalerClass {
+    new(...args: any[]) : Object
+}
+
+
+export class SignalerFactory {
+    static classes: {[name:string] : ISignalerClass } = {};
+    static register(name: string, iClass: ISignalerClass) {
+        if (!!iClass)
+            this.classes[name] = iClass;
+    }
+    static unRegister(name: string) {
+        delete this.classes[name];
+    }
+    static create(name?: string, ...args: any[]): ISignaler {        
+        name = !!name ? name : DefaultFactorySignalerName;
+        let instance: ISignaler;
+        let iClass = this.classes[name];
+        if (!!iClass) {
+            instance = new iClass(...args) as ISignaler;
+        }
+        return instance;
+    }
+
+}
+
+
+export class Signaler extends Client implements ISignaler {
     
 };
+
+SignalerFactory.register(DefaultFactorySignalerName, Signaler);
